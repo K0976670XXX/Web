e4f3f1a596dfee682ddef62824e5094c98fb33df
K0976670XXX <52744613+K0976670XXX@users.noreply.github.com>
2024-10-10 20:29:34 +0800

    Add files via upload

---
+++ data1005.js
@@ -0,0 +1,197 @@
+﻿var data = {
+  "coc_gift_pack": {
+  },
+  "coc_gift_pack_expired":{
+    "2024_免費寶箱_2":{
+          "time":"2024/09/19",
+          "url":"https://link.clashofclans.com/cnt/?action=voucher&code=53a84a01-ac52-4502-a7a0-0a0b70a7cd93",
+          "code":"53a84a01-ac52-4502-a7a0-0a0b70a7cd93",
+          "markdown":"免費的寶箱x3",
+          "img":[]
+    },
+    "2024_免費寶箱":{
+          "time":"2024/09/10",
+          "url":"https://link.clashofclans.com/en/?action=voucher&code=0c2f8d2b-6cbe-42ae-a169-e172cd1ec4d6",
+          "code":"0c2f8d2b-6cbe-42ae-a169-e172cd1ec4d6",
+          "markdown":"免費的寶箱x5",
+          "img":[]
+    },
+    "2024_9月迷你秀":{
+          "time":"2024/09/18",
+          "url":"https://link.clashofclans.com/en/?action=voucher&code=d3c7695b-682a-4d85-bf3d-50d479550961",
+          "code":"d3c7695b-682a-4d85-bf3d-50d479550961",
+          "markdown":"(img)[ice]x300 + (img)[Super_Medals]x100",
+          "img":['webp_img/ice.webp','webp_img/Super_Medals.webp']
+    },
+    "2024_8月_動漫與部落":{
+          "time":"2024/08/09",
+          "url":"https://link.clashofclans.com/cnt/?action=voucher&code=3d599d19-f313-4a2c-86db-013264bfdb5b",
+          "code":"3d599d19-f313-4a2c-86db-013264bfdb5b",
+          "markdown":"(img)[crystals]x150 + (img)[medals]x150",
+          "img":['webp_img/crystals.webp','webp_img/medals.webp']
+    },
+    "2024_7月_自然之戰":{
+          "time":"2024/07/27",
+          "url":"https://link.clashofclans.com/cnt/?action=voucher&code=a732966a-d7cb-46d6-b0e9-696fe919f1ae",
+          "code":"a732966a-d7cb-46d6-b0e9-696fe919f1ae",
+          "markdown":"(img)[鴨子澡盆] 或 (img)[寶石]x500<br>&emsp;&emsp;在自然之戰中選豬圈或未選的將獲得(img)[鴨子澡盆]",
+          "img":["webp_img/Duck_Bath.webp","webp_img/gem.webp","webp_img/Duck_Bath.webp"]
+      },
+      "2024_6月_超炸活動":{
+          "time":"2024/06/11",
+          "url":"https://link.clashofclans.com/cnt/?action=voucher&code=5bfda93c-942e-4586-81a1-5b1dbbf9f93b",
+          "code":"5bfda93c-942e-4586-81a1-5b1dbbf9f93b",
+          "markdown":"超級幣x150 和 冰塊x150",
+          "img":[]
+      },
+      "2024_5月哈蘭德活動_2":{
+          "time":"2024/05/20",
+          "url":"https://link.clashofclans.com/cnt/?action=voucher&code=ca89cd4a-2f86-44d3-b161-4a47ee257df3",
+          "code":"ca89cd4a-2f86-44d3-b161-4a47ee257df3",
+          "markdown":"(img)[金靴]x270",
+          "img":["webp_img/Golden_Boot.webp"]
+      },
+      "2024_5月哈蘭德活動":{
+          "time":"2024/05/07",
+          "url":"https://link.clashofclans.com/en/?action=voucher&code=310fa50c-7f22-4afd-bbcb-a8be48498a50",
+          "code":"310fa50c-7f22-4afd-bbcb-a8be48498a50",
+          "markdown":"(img)[金靴]x100",
+          "img":["webp_img/Golden_Boot.webp"]
+      },
+      "2024_04_23裝飾品":{
+          "time":"2024/04/23",
+          "url":"https://link.clashofclans.com/cnt/?action=voucher&code=c4fda901-9728-4a46-9486-a025f728ffb2",
+          "code":"c4fda901-9728-4a46-9486-a025f728ffb2",
+          "markdown":"(img)[裝飾品]x1",
+          "img":["webp_img/Crown_Sword.webp"]
+      },
+      "壁壘之戒2024_03_29":{
+          "time":"2024/03/29",
+          "url":"https://link.clashofclans.com/cnt/?action=voucher&code=0c08fdda-524b-4e88-b29d-000e5eb9cb46",
+          "code":"0c08fdda-524b-4e88-b29d-000e5eb9cb46",
+          "markdown":"(img)[壁壘之戒]x1",
+          "img":["webp_img/Wall_Ring.webp"]
+      },
+      "礦石20240328":{
+          "time":"2024/03/28",
+          "url":"https://link.clashofclans.com/cn/?action=voucher&code=f08716c3-02b7-4269-8d38-4eb3ea9c73ce",
+          "code":"f08716c3-02b7-4269-8d38-4eb3ea9c73ce",
+          "markdown":"(img)[藍礦]x500,(img)[紫礦]x100",
+          "img":["webp_img/Shiny_Ore.webp","webp_img/Glowy_Ore.webp"]
+      },
+      "訓練藥水20240324":{
+            "time":"2024/03/24",
+            "url":"https://link.clashofclans.com/cnt/?action=voucher&code=fe096a17-8b25-4fd6-9d0b-a58228bbe67d",
+            "code":"fe096a17-8b25-4fd6-9d0b-a58228bbe67d",
+            "markdown":"(img)[訓練藥水]x1",
+            "img":["webp_img/Training_Potion.webp"]
+      },
+      "礦石20240323":{
+            "time":"2024/03/23",
+            "url":"https://link.clashofclans.com/cnt/?action=voucher&code=2bd74de0-8e8a-4edb-9a04-02b7b6e949d4",
+            "code":"2bd74de0-8e8a-4edb-9a04-02b7b6e949d4",
+            "markdown":"(img)[藍礦]x1000",
+            "img":["webp_img/Shiny_Ore.webp"]
+      },
+  },
+  
+  "useful_web":{
+    "查看玩家科技資訊(無聊的村庄建置)":{
+        "url":"tool/COC_Get_Info.html",
+        "img":"",
+        "name":"查看玩家科技資訊"
+      },
+    "礦石計算機(無聊的村庄建置)":{
+        "url":"tool/礦石計算機.html",
+        "img":"",
+        "name":"礦石計算機"
+      },
+    "查看部落詳細捐收狀況(無聊的村庄建置)":{
+      "url":"tool/部落捐收狀況.html",
+      "img":"",
+      "name":"查看部落詳細捐收狀況"
+    },
+    "部落管理(無聊的村庄建置)":{
+      "url":"tool/Clans_Manage.html",
+      "img":"",
+      "name":"查看捐收狀況及是否有打都城"
+    },
+    "陣型複製":{
+        "url":"https://www.clashchamps.com/i-need-a-base/",
+        "img":"img/clash-champs-logo-100.png",
+        "name":"clashchamps"
+    },
+    "部落配兵查詢":{
+        "url":"https://clasharmies.com/armies",
+        "img":"img/ClashArmies.png",
+        "name":"ClashArmies"
+    },
+    "部落商店":{
+        "url":"https://store.supercell.com/game/clashofclans",
+        "img":"img/supercell_store.jpg",
+        "name":"Supercell Store"
+      },
+    "玩家及部落資料查詢":{
+        "url":"https://www.clashofstats.com/cn/clans/2GCVULGVP/summary",
+        "img":"img/logo-home-blue-grey-2x.png",
+        "name":"clash of stats"
+      },
+    "數據查訊(簡中)":{
+        "url":"https://clashpost.com/",
+        "img":"img/apple-touch-icon.png",
+        "name":"部落驿站"
+      },
+    "遊戲統計數據":{
+        "url":"https://clashspot.net/en/stats/players",
+        "img":"img/clashspot.webp",
+        "name":"clashspot"
+      },
+    "升級跟蹤器":{
+      "url":"https://www.clash.ninja/upgrade-tracker",
+      "img":"img/clashninja.webp",
+      "name":"clash ninja"
+    },
+    "升級跟蹤器":{
+      "url":"https://www.clash.ninja/upgrade-tracker",
+      "img":"img/clashninja.webp",
+      "name":"clash ninja"
+    },
+    "買陣、買教學":{
+      "url":"https://blueprintcoc.com/",
+      "img":"img/blueprintcoc.webp",
+      "name":"BluePrint coc"
+    },
+    "買帳號(國外)":{
+      "url":"https://www.g2g.com/categories/coc1-account-for-sale?sort=recommended",
+      "img":"img/g2g.png",
+      "name":"g2g"
+    },
+    "世界錦標賽賽事網站":{
+        "url":"https://event.supercell.com/clashofclans/en",
+        "img":"",
+        "name":"COC World Championship"
+      },
+    "數據查訊(英文)":{
+        "url":"https://clashofclans.fandom.com/wiki/Cumulative_Costs/Home_Village",
+        "img":"img/Site-logo.png",
+        "name":"Clash of Clans Wiki"
+      },
+    "數據查訊(多語言)":{
+        "url":"https://coc.guide/cnt",
+        "img":"img/coc_Guide.jpg",
+        "name":"Clash Of Clans Guide"
+      },
+    "部落衝突API":{
+        "url":"https://developer.clashofclans.com/#/documentation",
+        "img":"",
+        "name":"developer clashofclans"
+      }
+    
+  },
+  "info":{
+    "新裝備資訊":"info/新裝備資訊.html"
+  },
+  "old_info":{
+    "2024/9月更新完整內容":"info/old/九月更新完整內容.html"
+  }
+};
